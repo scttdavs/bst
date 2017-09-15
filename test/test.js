6310bb7eb2fb46e3bc786a17f98722d83bacd466
Scott Davis <sc.ttdav.s@gmail.com>
2017-09-15 15:25:35 -0400

    add more tests

--- test/test.js
+++ test/test.js
@@ -1,11 +1,11 @@
 "use strict";
 
-const assert = require('assert');
+const assert = require("assert");
 const Node = require("../bst");
 
-describe('BST', () => {
-  describe('Contains', () => {
-    it('should return true if it contains a value anywhere in the tree', () => {
+describe("BST", () => {
+  describe("Contains", () => {
+    it("should return true if it contains a value anywhere in the tree", () => {
       const node = new Node(1);
       node.insert(5).insert(50);
 
@@ -14,11 +14,34 @@ describe('BST', () => {
       assert.equal(node.contains(50), true);
     });
 
-    it('should return false if it does NOT contain a value anywhere in the tree', () => {
+    it("should return false if it does NOT contain a value anywhere in the tree", () => {
       const node = new Node(1);
       node.insert(5).insert(50);
 
       assert.equal(node.contains(10), false);
+    });
+  });
+
+  describe("Insert", () => {
+    it("should insert the value on the correct 'side' of the node", () => {
+      const node = new Node(5);
+      node.insert(10).insert(1);
+
+      assert.equal(node.left.value, 1);
+      assert.equal(node.right.value, 10);
+    });
+  });
+
+  describe("Delete", () => {
+    it("should delete the value anywhere in the tree", () => {
+      const node = new Node(5);
+      node.insert(10).insert(1);
+
+      assert.equal(node.contains(1), true);
+
+      node.delete(1);
+
+      assert.equal(node.contains(1), false);
     });
   });
 });
